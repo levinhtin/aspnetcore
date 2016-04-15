5f3fb96f74a06dbb26925fac79393a41fb15a0fd
Tin Le <Tin Le>
2016-04-16 01:36:33 +0700

    add project WEBAPI with authentication using Json Web Token and UserIdentity

---
+++ src/WEBAPI/Repository/IArtcleRepository.cs
@@ -0,0 +1,16 @@
+﻿using WEBAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WEBAPI.Repository
+{
+    public interface IArticleRepository
+    {
+        IEnumerable<Article> AllArticle { get; }
+        void Add(Article item);
+        Article GetById(int id);
+        bool Delete(int id);
+    }
+}
