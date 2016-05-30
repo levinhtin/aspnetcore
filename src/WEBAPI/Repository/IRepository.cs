ddc78c2d0c4148d7ad0e5b4f7449e57ef772d942
Tin Le <Tin Le>
2016-05-30 20:12:52 +0700

    feat(webapi): add repository
    add repository for webapi

--- src/WEBAPI/Repository/IRepository.cs
+++ src/WEBAPI/Repository/IRepository.cs
@@ -3,23 +3,22 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics.Contracts;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace WEBAPI.Repository
 {
-    [ContractClass(typeof(IRepositoryContract<>))]
-    public interface IRepository<T> : IReadOnlyRepository<T> where T : class
+    public interface IRepository<T> where T : class
     {
+        T Get(int id);
+        IEnumerable<T> GetAll();
+        IEnumerable<T> Find(Expression<Func<T, bool>> predicate);
 
-        void Add(T item);
+        void Add(T entity);
+        void AddRange(IEnumerable<T> entities);
 
-        void Remove(T item);
-
-        void Update(T item);
-
-        void DiscardChanges();
-
-        Task SaveChangesAsync(CancellationToken cancellationToken);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
     }
 }
