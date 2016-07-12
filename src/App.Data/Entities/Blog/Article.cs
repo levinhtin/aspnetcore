1c6597901498bd8db3cf223ae4e42e20e4ac8b1c
TinLe - Home <levinhtin@gmail.com>
2016-07-12 21:46:37 +0700

    model generate

--- src/App.Data/Entities/Blog/Article.cs
+++ src/App.Data/Entities/Blog/Article.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +9,14 @@ namespace App.Data.Entities.Blog
 {
     public class Article : AbstractModel
     {
+        [Required]
+        [StringLength(200)]
         public string Title { get; set; }
 
+        [StringLength(200)]
         public string Summary { get; set; }
 
+        [Column(TypeName = "text")]
         public string Content { get; set; }
 
         public virtual Category Category { get; set; }
